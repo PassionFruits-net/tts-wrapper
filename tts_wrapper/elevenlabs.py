b3b9c16774a514dbbb96b74ad947088bd7c4f088
Egil <egil.moller@freecode.no>
2024-10-24 11:48:05 +0200

    Bugfix for get_voices / client == None

--- tts_wrapper/elevenlabs.py
+++ tts_wrapper/elevenlabs.py
@@ -2,16 +2,20 @@ from elevenlabs.client import ElevenLabs
 from pydub import AudioSegment
 from io import BytesIO
 
+_client = None
+def get_client():
+    global _client
+    if _client is None:
+        _client = ElevenLabs()
+    return _client
+
 def get_voices():
+    client = get_client()
     voices = client.voices.get_all().voices
     return {voice.name: {"title": voice.name, "properties": {}} for voice in voices}
 
-client = None
 def render(text, voice, **kw):
-    global client
-    if client is None:
-        client = ElevenLabs()
-
+    client = get_client()
     model = "eleven_multilingual_v2"
     tts_response = client.generate(
         text=text,
